63424bda9b0b9d94eff9cce444e4deebfea17c47
Ivan Docenko <decel1337@gmail.com>
2022-12-03 20:31:45 +0300

    movies-explorer-api

--- routes/movies.js
+++ routes/movies.js
@@ -1,8 +1,39 @@
 const router = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 const { createMovie, getLikeMovies, deliteMovieById } = require('../controllers/movies');
+const BadRequestError = require('../errors/BadRequestError');
 
-router.post('/movies', createMovie);
+router.post('/movies', celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.number().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().custom((image) => {
+      if (!validator.isURL(image)) {
+        throw new BadRequestError('Это не URL');
+      }
+      return image;
+    }),
+    trailerLink: Joi.string().required().custom((trailerLink) => {
+      if (!validator.isURL(trailerLink)) {
+        throw new BadRequestError('Это не URL');
+      }
+      return trailerLink;
+    }),
+    thumbnail: Joi.string().required().custom((thumbnail) => {
+      if (!validator.isURL(thumbnail)) {
+        throw new BadRequestError('Это не URL');
+      }
+      return thumbnail;
+    }),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+}), createMovie);
 
 router.get('/movies', getLikeMovies);// пока возвращет все фильмы, а надо
 // ( возвращает все сохранённые текущим  пользователем фильмы )
