9183ce7724ac0f2f4c8984fdaa86a3895e398b8d
LODJEN2002 <decel1337@gmail.com>
2023-02-09 14:12:01 +0300

    fix

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ mongoose.connect(MONGO_URL, { autoIndex: true });
 
 app.use(requestLogger);
 
-// const allowedCors = [
-//   'http://movies.frontend.nomoredomainsclub.ru',
-//   'http://localhost:3000',
-//   'http://localhost:3001',
-// ];
+const allowedCors = [
+  'http://movies.frontend.nomoredomainsclub.ru',
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
 
 app.use((req, res, next) => {
   const { origin } = req.headers;
@@ -27,13 +27,15 @@ app.use((req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  if ('http://movies.frontend.nomoredomainsclub.ru'.includes(origin)) {
+  if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
     return res.end();
   }
   return next();
