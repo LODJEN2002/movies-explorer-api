9540535bcd16a14edc6f4f7c37f4612ed267be78
LODJEN2002 <decel1337@gmail.com>
2023-02-11 00:19:22 +0300

    off cros

--- app.js
+++ app.js
@@ -15,32 +15,35 @@ mongoose.connect(MONGO_URL, { autoIndex: true });
 
 app.use(requestLogger);
 
-// const allowedCors = [
-//   'http://movies.frontend.nomoredomainsclub.ru',
-//   'https://movies.frontend.nomoredomainsclub.ru',
-//   'http://localhost:3000',
-//   'http://localhost:3001',
-// ];
+const allowedCors = [
+  'http://movies.frontend.nomoredomainsclub.ru',
+  'https://movies.frontend.nomoredomainsclub.ru',
 
-// app.use((req, res, next) => {
-//   const { origin } = req.headers;
-//   const { method } = req;
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   const requestHeaders = req.headers['access-control-request-headers'];
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
 
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', true);
-//   }
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     res.header('Access-Control-Allow-Origin', origin);
-//     res.header('Access-Control-Allow-Credentials', true);
-//     return res.end();
-//   }
-//   return next();
-// });
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    console.log('allowedCors');
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    console.log('поподает в опции');
+    return res.end();
+  }
+  return next();
+});
 
 app.use(router);
 
